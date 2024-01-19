f6536d222110e830f911480c5ff9f2d03ee6fbb1
johnahumanbeing <mseewak@gmail.com>
2024-01-19 21:44:32 +0300

    puppet manifest that installs the package flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,8 +2,12 @@
 
 package { 'flask':
   ensure    => '2.1.0',
-  provider => 'pip3',
-  name    => 'flask'
+  provider  => 'pip3',
+  name      => 'flask',
 }
 
-
+package { 'werkzeug':
+  ensure    => '2.1.1',
+  provider  => 'pip3',
+  name      => 'werkzeug',
+}
