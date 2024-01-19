d82e7805e0730f19674f4965fb391c720655ddda
johnahumanbeing <mseewak@gmail.com>
2024-01-19 23:16:42 +0300

    package flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,13 +8,13 @@ package { 'python3-pip':
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
-  provider => 'pip3',
+  provider => 'pip',
   require  => Package['python3-pip'],
 }
 
 package { 'werkzeug':
   ensure    => '2.1.1',
-  provider  => 'pip3',
+  provider  => 'pip',
   name      => 'werkzeug',
   require   => Package['python3-pip'],
 }
