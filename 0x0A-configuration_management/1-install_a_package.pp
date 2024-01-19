a49e9369836bced0dc7f50e6cd41aedc1a6e90e6
johnahumanbeing <mseewak@gmail.com>
2024-01-19 23:01:47 +0300

    a manifest that installs package flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,18 @@
+#!/usr/bin/pup
 # a manifest that installs package flask
+
+package { 'python3-pip':
+  ensure => installed,
+}
 
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
   provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure    => '2.1.1',
+  provider  => 'pip3',
+  name      => 'werkzeug',
 }
