5a2093b8b172732e124808bc70059eddef44f0a1
johnahumanbeing <mseewak@gmail.com>
2024-01-19 21:10:15 +0300

    puppet manifest that creates a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# A puppet manifest to create a fie in /tmp
+
+file { 'school':
+ensure  => file,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+path    => '/tmp/school'
+}
