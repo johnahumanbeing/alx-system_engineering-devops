923a9520ea407ba7df5dc69e8115c045c447529a
johnahumanbeing <mseewak@gmail.com>
2024-01-19 22:15:50 +0300

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
 # manifest that installs package flask
 
 package { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
-  name      => 'flask',
-}
-
-package { 'werkzeug':
-  ensure    => '2.1.1',
-  provider  => 'pip3',
-  name      => 'werkzeug',
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
